5f4176a962e4a912f114dfdaa5182b4b827334b5
flowlike6ixgod <flowlike6ixgod@gmail.com>
2022-06-29 19:10:57 +0300

    Created new PlayerController class

---
+++ Source/Villach/Public/Character/VillachPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "VillachPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VILLACH_API AVillachPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
